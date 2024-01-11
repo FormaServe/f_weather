52cc5d7bde397ec3d62263234a3faf24f15c2100
AndyYouens <andy@formaserve.co.uk>
2024-01-11 17:09:24 +0000

    apikey probs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 const superagent = require('superagent')
 const debug = require('debug')('weather:index')
+const dotenv = require('dotenv');
+dotenv.config();
+
 const APIKEY = process.env.APIKEY
 
 let title = 'FormaServe - f_Weather'
@@ -22,8 +25,9 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res) {
   debug('--> into index POST')
-  let city = `${req.body.city},uk`
+  debug(`     API Key is ${APIKEY}`)
 
+  let city = `${req.body.city},uk`
   let url = `http://api.openweathermap.org/data/2.5/weather`
 
   // Use SuperAgent to make the request
@@ -41,14 +45,15 @@ router.post('/', function(req, res) {
           error: 'Error, please try again'
         })
       } else {
-        debug(`Weather body: {result.body}`)
+        debug(`Weather body: ${JSON.stringify(result.body, null, 2)}`)
+        
         if (result.body.main == undefined) {
           result.render('index', {
             weather: null,
             error: 'Error, please try again'
           })
         } else {
-          debug(`Temp is: {result.body.main.temp}`)
+          debug(`Temp is: ${result.body.main.temp}`)
           let weatherText = `Weather is ${result.body.weather[0].main},
             (${result.body.weather[0].description})
             ${result.body.main.temp} degrees C in ${result.body.name}!`
